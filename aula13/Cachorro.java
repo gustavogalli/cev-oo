b080621a0adf208d09d40d9d1bba196a93e69964
Gustavo Galli <gustavosgalli@gmail.com>
2021-11-25 18:56:21 -0300

    Finalizando com aulas 12 e 13

---
+++ aula13/Cachorro.java
@@ -0,0 +1,13 @@
+package oo.aula13;
+
+public class Cachorro {
+	
+	public void reagir(String palavra) {
+		System.out.println("Eu ouvi " + palavra);
+	}
+	
+	public void reagir(int numero) {
+		System.out.println("Você disse o número " + numero);
+	}
+
+}
---
+++ aula13/Main.java
@@ -0,0 +1,8 @@
+package oo.aula13;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+}
